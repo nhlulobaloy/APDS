3b7acb1e76ae982facca447ae7f5b882707a7678
nhlulobaloy <131444906+nhlulobaloy@users.noreply.github.com>
2026-05-01 22:19:43 +0200

    resolving comflites

--- backend/Services/authServices.js
+++ backend/Services/authServices.js
@@ -1,29 +1,14 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-<<<<<<< HEAD
-
-//password hashing using bcrypt 
-=======
->>>>>>> person3-security
 export const hashPassword = async (password) => {
 const hashedPassword = await bcrypt.hash(password, 10);
 return hashedPassword;
 } 
-<<<<<<< HEAD
-//generates the token
-=======
-
->>>>>>> person3-security
 export const generateToken = async (userId) => {
     const token = jwt.sign({  userId }, process.env.SECRET_KEY, {expiresIn: "5h"});
     return token;
 }
-<<<<<<< HEAD
-//verify the user password
-=======
-
->>>>>>> person3-security
 export const verifyPassword = async (userPassword, dbPassword) => {
     const isValid = await bcrypt.compare(userPassword, dbPassword);
     return isValid;
--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import express from 'express';
-import pool from "./config/db.js";
-import cors from 'cors';
-import { hashPassword, verifyPassword, generateToken} from './Services/authServices.js';
-import { verifyToken } from './middleware/authMiddleware.js';
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-const PORT = process.env.PORT || 5000;
-
-
-//SignUp 
-app.post('/signup', async (req, res) => {
-    const { fullNames, idNumber, accountNumber, password } = req.body;
-    const hashedPassword = await hashPassword(password);// hash the password before inserting it 
-    try {
-
-        const sql = `INSERT INTO users(Full_name, id_number, account_number, password) VALUES(?, ?, ?, ?)`; 
-        const [results] = await pool.query(sql, [fullNames, idNumber, accountNumber, hashedPassword]);  
-        res.status(200).json({ message: "Account creation was succesful" })  
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: "Account Creation error, no duplication allowed" });
-    }
-})
- 
-//login
-app.post('/login', async (req, res) => {
-    const { idNumber, password } = req.body;
-    const [result] = await pool.query(`SELECT id, password FROM users WHERE id_number = ?`, [idNumber]);
-=======
 import dotenv from "dotenv";
 dotenv.config({ override: true });
 
@@ -146,39 +113,11 @@ app.post('/login', async (req, res) => {
 
     console.log("LOGIN RESULT:", result);
 
->>>>>>> person3-security
     if (result.length <= 0) {
         return res.status(404).json({ message: "User does not exist" });
     }
 
     const dbPassword = result[0].password;
-<<<<<<< HEAD
-    const userId= result[0].id;
-
-    const isValid = await verifyPassword(password, dbPassword);
-    if(isValid) {
-        const token = await generateToken(userId)
-        return res.status(200).json({message: "success", token: token})
-    } else{
-        return res.status(404).json({message: "Password incorrect"})
-    }
-})
-
-//dashboard
-app.get('/dashboard', verifyToken, (req, res) => {
-    res.json({message: `Welcome user ${req.userId}`});  
-});
-app.post('/payment', verifyToken, async (req, res) => {
-    const { paymentAmount, currency, provider, payeeAccountNumber, swiftCode } = req.body;
-
-    try {
-        if (!paymentAmount || !currency || !provider || !payeeAccountNumber || !swiftCode) {
-            return res.status(400).json({ message: "All payment fields are required" });
-        }
-
-        const sql = `
-            INSERT INTO transactions 
-=======
     const userId = result[0].id;
 
     const isValid = await verifyPassword(password, dbPassword);
@@ -243,7 +182,6 @@ app.post('/payment', verifyToken, async (req, res) => {
 
         const sql = `
             INSERT INTO transactions
->>>>>>> person3-security
             (payment_amount, currency, provider, payee_account_number, swift_code)
             VALUES (?, ?, ?, ?, ?)
         `;
@@ -256,20 +194,6 @@ app.post('/payment', verifyToken, async (req, res) => {
             swiftCode
         ]);
 
-<<<<<<< HEAD
-        res.status(200).json({ message: "Payment submitted successfully" });
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Payment submission failed" });
-    }
-});
-
-//the app port
-app.listen(PORT, () => {
-    console.log(`Server running and it is listing to port ${PORT}`); 
-}); 
-=======
         res.status(200).json({
             message: "Payment submitted successfully"
         });
@@ -288,4 +212,3 @@ app.listen(PORT, () => {
 app.listen(PORT, () => {
     console.log(`Server running and it is listing to port ${PORT}`);
 });
->>>>>>> person3-security
